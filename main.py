9a5620f7c2b8ab428fad0911d63b74bdb8ed9278
Andrew Sepulveda <streak324@gmail.com>
2023-07-16 19:22:15 -0700

    checkpoint

--- main.py
+++ main.py
@@ -26,11 +26,16 @@ PIXELS_PER_WORLD_UNITS = 10
 #world units
 HURTBOX_CAPSULE_RADIUS = 72 / PIXELS_PER_WORLD_UNITS
 #world units
-HURTBOX_CAPSULE_LENGTH = 16 / PIXELS_PER_WORLD_UNITS
+HURTBOX_CAPSULE_STRETCH_LENGTH= 16 / PIXELS_PER_WORLD_UNITS
 
 HURTBOX_COLOR = (171, 174, 105, 128)
 
 TIMESTEP = 1/60
+
+FIGHTER_COLLISION_TYPE = 1
+PLATFORM_COLLISION_TYPE = 2
+
+FALL_VELOCITY = 25.0
 
 def draw_capsule_world_to_pixel(x: float, y: float, radius, stretch_h: float, color: arcade.Color):
 	draw_capsule(x*PIXELS_PER_WORLD_UNITS, y*PIXELS_PER_WORLD_UNITS, radius*PIXELS_PER_WORLD_UNITS, stretch_h*PIXELS_PER_WORLD_UNITS, color)
@@ -40,21 +45,48 @@ def draw_capsule(x: float, y: float, radius: float, stretch_h: float, color: arc
 	arcade.draw_arc_filled(x, y+stretch_h*0.5, 2*radius, 2*radius, color, start_angle=0, end_angle=180)
 	arcade.draw_arc_filled(x, y-stretch_h*0.5, 2*radius, 2*radius, color, start_angle=180, end_angle=360)
 
-def do_nothing(arb, space, data) -> bool:
-	print("collision!")
-	return False
-
 class Platform(): 
 	def __init__(self, space: pymunk.Space, min: tuple[float,float], max: tuple[float,float], physics_color: arcade.Color):
 		b = pymunk.Body(body_type = pymunk.Body.STATIC)
-		b._set_position(((min[0]+max[0])*0.5, (min[1]+max[1])*0.5))
-		shape = pymunk.Segment(b, (0, max[1]), (0, min[1]), max[1]-min[0])
+		b._set_position((0, 0))
+		shape = pymunk.Segment(b, ((min[0]+max[0])*0.5, max[1]), ((min[0]+max[0])*0.5, min[1]), max[0]-min[0])
+		shape.collision_type = PLATFORM_COLLISION_TYPE
 		space.add(b,shape)
 		self.min = min
 		self.max = max
 		self.physics_color = physics_color
+		shape.friction = 1.0
 	def draw_physics(self, pixels_per_world_units):
 		arcade.draw_lrtb_rectangle_filled(self.min[0]*pixels_per_world_units, self.max[0]*pixels_per_world_units, self.max[1]*pixels_per_world_units, self.min[1]*pixels_per_world_units, self.physics_color)
+
+class Fighter():
+	def __init__(self, space: pymunk.Space, center: tuple[float, float]):
+		#hurtbox body is supposed to be the shape of a capsule: 2 circles and 1 rectangle
+		self.hurtbox_body = pymunk.Body(mass=1, moment=float("inf"))
+		self.hurtbox_body._set_position(center)
+		self.hurtbox_c1 = pymunk.Circle(self.hurtbox_body, HURTBOX_CAPSULE_RADIUS, offset=(0, HURTBOX_CAPSULE_STRETCH_LENGTH*0.5))
+		#self.hurtbox_c1.filter = pymunk.ShapeFilter(categories=0b1, mask=0b10)
+		self.hurtbox_c1.collision_type = FIGHTER_COLLISION_TYPE
+		self.hurtbox_c2 = pymunk.Circle(self.hurtbox_body, HURTBOX_CAPSULE_RADIUS, offset=(0, -HURTBOX_CAPSULE_STRETCH_LENGTH*0.5))
+		#self.hurtbox_c2.filter = pymunk.ShapeFilter(categories=0b1, mask=0b10)
+		self.hurtbox_c2.collision_type = FIGHTER_COLLISION_TYPE
+		self.hurtbox_box = pymunk.Poly.create_box(self.hurtbox_body, (2*HURTBOX_CAPSULE_RADIUS, HURTBOX_CAPSULE_STRETCH_LENGTH))
+		self.hurtbox_box.collision_type = FIGHTER_COLLISION_TYPE
+		space.add(self.hurtbox_body, self.hurtbox_c1, self.hurtbox_c2, self.hurtbox_box)
+
+	def draw_physics(self, pixels_per_world_units):
+		bodyp = self.hurtbox_body._get_position()
+		c1p = self.hurtbox_c1.center_of_gravity.__add__(bodyp)
+		arcade.draw_arc_filled(c1p.x*pixels_per_world_units, c1p.y*pixels_per_world_units, 2*HURTBOX_CAPSULE_RADIUS*pixels_per_world_units, 2*HURTBOX_CAPSULE_RADIUS*pixels_per_world_units, HURTBOX_COLOR, start_angle=0, end_angle=180)
+		c2p = self.hurtbox_c2.center_of_gravity.__add__(bodyp)
+		arcade.draw_arc_filled(c2p.x*pixels_per_world_units, c2p.y*pixels_per_world_units, 2*HURTBOX_CAPSULE_RADIUS*pixels_per_world_units, 2*HURTBOX_CAPSULE_RADIUS*pixels_per_world_units, HURTBOX_COLOR, start_angle=180, end_angle=360)
+		boxp = self.hurtbox_box.center_of_gravity.__add__(bodyp)
+		arcade.draw_rectangle_filled(boxp.x*pixels_per_world_units, boxp.y*pixels_per_world_units, 2*HURTBOX_CAPSULE_RADIUS*pixels_per_world_units, HURTBOX_CAPSULE_STRETCH_LENGTH*pixels_per_world_units, HURTBOX_COLOR)
+		pass
+
+def always_collide(arb: pymunk.Arbiter, space: pymunk.Space, data: any) -> bool:
+	print("hi!!!")
+	return True
 
 class MyGameWindow(arcade.Window):
 	def __init__(self, width, height, title):
@@ -62,24 +94,14 @@ class MyGameWindow(arcade.Window):
 		self.input = numpy.zeros(4)
 		self.prev_input = numpy.zeros(4)
 		self.physics_sim = pymunk.Space()
-		self.physics_sim._set_gravity((0,-10))
+		self.physics_sim._set_gravity((0,-100))
 
-		self.fighter_body = pymunk.Body(mass=1, moment=10)
-		self.fighter_body._set_position((20, 40))
-		c1 = pymunk.Circle(self.fighter_body, HURTBOX_CAPSULE_RADIUS, offset=(0, HURTBOX_CAPSULE_LENGTH*0.5))
-		c1.collision_type = 1
-		c1.filter = pymunk.ShapeFilter(group=1)
-		c2 = pymunk.Circle(self.fighter_body, HURTBOX_CAPSULE_RADIUS, offset=(0, -HURTBOX_CAPSULE_LENGTH*0.5))
-		c2.collision_type = 1
-		c2.filter = pymunk.ShapeFilter(group=1)
-		seg = pymunk.Segment(self.fighter_body, (0., -HURTBOX_CAPSULE_LENGTH*0.5), (0., HURTBOX_CAPSULE_LENGTH*0.5), HURTBOX_CAPSULE_RADIUS)
-		seg.collision_type = 1
-		seg.filter = pymunk.ShapeFilter(group=1)
-		self.physics_sim.add(self.fighter_body, c1,c2,seg)
-		handler = self.physics_sim.add_collision_handler(1, 1)
-		handler.begin = do_nothing
+		self.fighter = Fighter(self.physics_sim, (20,40))
 
 		self.platform = Platform(self.physics_sim, (10,10), (110,30), arcade.csscolor.YELLOW)
+
+		handler = self.physics_sim.add_collision_handler(FIGHTER_COLLISION_TYPE, PLATFORM_COLLISION_TYPE)
+		handler.begin = always_collide
 
 		super().__init__(width, height, title)
 		arcade.set_background_color(arcade.color.ALMOND)
@@ -91,14 +113,16 @@ class MyGameWindow(arcade.Window):
 
 	def on_draw(self):
 		self.clear()
-		draw_capsule_world_to_pixel(self.fighter_body._get_position().x,self.fighter_body._get_position().y,HURTBOX_CAPSULE_RADIUS,HURTBOX_CAPSULE_LENGTH,HURTBOX_COLOR)
 		self.platform.draw_physics(PIXELS_PER_WORLD_UNITS)
+		self.fighter.draw_physics(PIXELS_PER_WORLD_UNITS)
+		#pos = self.fighter.hurtbox_body._get_position()
+		#draw_capsule_world_to_pixel(pos.x,pos.y,HURTBOX_CAPSULE_RADIUS,HURTBOX_CAPSULE_STRETCH_LENGTH,HURTBOX_COLOR)
 
 	def on_update(self, delta_time):
-		dx = ((float)((self.input[INPUT_MOVE_SIDE_PRECEDENCE] != SIDE_PRECEDENCE_LEFT or self.input[INPUT_MOVE_LEFT] == False) and self.input[INPUT_MOVE_RIGHT]) \
+		dx = 50 * ((float)((self.input[INPUT_MOVE_SIDE_PRECEDENCE] != SIDE_PRECEDENCE_LEFT or self.input[INPUT_MOVE_LEFT] == False) and self.input[INPUT_MOVE_RIGHT]) \
 			- \
 			(float)((self.input[INPUT_MOVE_SIDE_PRECEDENCE] != SIDE_PRECEDENCE_RIGHT or self.input[INPUT_MOVE_RIGHT] == False) and self.input[INPUT_MOVE_LEFT]))
-		self.fighter_body.apply_impulse_at_local_point((dx, 0))
+		self.fighter.hurtbox_body.velocity = (dx, max(self.fighter.hurtbox_body.velocity.y, -FALL_VELOCITY))
 		self.prev_input = self.input.copy()
 		self.physics_sim.step(TIMESTEP)
 
