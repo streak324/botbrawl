c2a9ac190798bd3262ed35119257b074aaf113fc
streak324 <streak324@gmail.com>
2023-09-10 16:57:34 -0700

    fix attack test

--- attack_test.py
+++ attack_test.py
@@ -14,7 +14,11 @@ class TestStepAttackFunction(unittest.TestCase):
 		tests = [
 			{
 				"name": "Test with 2 startup and 3 active frames",
-				"attack": Attack(powers = [Power(casts=[Cast(startup_frames=2, active_frames=3, hitbox=Hitbox(left_shapes=[], right_shapes=[]))])], name="dummy_attack"),
+				"attack": Attack(
+					powers = [ Power(casts=[Cast(min_startup_frames=2, active_frames=3, hitbox=Hitbox(left_shapes=[], right_shapes=[]))])], 
+					name="dummy_attack",
+					is_attack_triggered_func=lambda is_fighter_grounded,input: True
+					),
 				"number_of_trues": 4  # expected number of times that function will return true
 			},
 			{
@@ -22,17 +26,17 @@ class TestStepAttackFunction(unittest.TestCase):
 				"attack": Attack([
 					Power(
 						casts = [
-							Cast(startup_frames=2, active_frames=2, move_velocity=(50,0), is_velocity_on_active_frames_only=True), 
-							Cast(startup_frames=3, active_frames=2, move_velocity=(100,10), is_velocity_on_active_frames_only=True),
+							Cast(min_startup_frames=2, active_frames=2, velocity=(50,0), is_velocity_on_active_frames_only=True), 
+							Cast(min_startup_frames=3, active_frames=2, velocity=(100,10), is_velocity_on_active_frames_only=True),
 							Cast(
-								startup_frames=1, active_frames=4, velocity=(100,0), is_velocity_on_active_frames_only=False, base_dmg = 13, var_force=20, fixed_force=80,
+								min_startup_frames=1, active_frames=4, velocity=(100,0), is_velocity_on_active_frames_only=False, base_dmg = 13, var_force=20, fixed_force=80,
 								hitbox=Hitbox( [], [], ),
 							)
 						],
 						cooldown_frames = 10, stun_frames = 18
 					), 
-					Power([Cast(startup_frames=0, active_frames=1, velocity=(100,0))], fixed_recovery_frames = 2, recovery_frames = 18) 
-				], name="unarmed_side_light"),
+					Power([Cast(min_startup_frames=0, active_frames=1, velocity=(100,0))], fixed_recovery_frames = 2, recovery_frames = 18) 
+				], name="unarmed_side_light", is_attack_triggered_func=lambda is_fighter_grounded,input: True),
 				"number_of_trues": 12
 			},
 			# Add more test cases as required
